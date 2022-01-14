4e84b36daf2086309c5ed28dc015d6a7a62cbd16
Rebekah 2021 <rebekahmcdevitt@gmail.com>
2022-01-14 09:30:06 -0500

    update js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,22 @@
+// function for submit btn
 
-// fetch open weather api
-fetch("api.openweathermap.org/data/2.5/weather?q={city name}&appid={65b56975875192b114dbc4d00909c453}")
+$(document).ready(function () {
+    $("#submit").click(function () {
+        var cityEl = $(this).parent().siblings("input").val()
+        
+        // fetch open weather api
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="+cityEl+"&appid=65b56975875192b114dbc4d00909c453")
+            .then(response => response.json())
+            .then(data => {
+                getCityInfo(data.coord.lat, data.coord.lon)
+            });
+    });
+});
+function getCityInfo(lat,lon) {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&units=imperial&appid=65b56975875192b114dbc4d00909c453")
     .then(response => response.json())
-    .then(data => console.log(data));
-
-// Add eventlistener btn for submit
-
+    .then(data => {
+    
+    });
+};
+ getCityInfo();
